36483df423e00ffee72050d3dec418c3516bba4e
Aswin Dhakal <53301205+dhakalaswin@users.noreply.github.com>
2024-11-11 21:52:51 +0545

    File Handling Read Module

--- file.js
+++ file.js
@@ -5,4 +5,8 @@ const fs = require('fs');
 
 
 //Async
-fs.writeFile("./test.txt", 'Welcome to Node Js', (err) => { });
+// fs.writeFile("./test.txt", 'Welcome to Node Js', (err) => { });
+
+// Read Command
+const result = fs.readFileSync("./contact.txt", "utf-8");
+console.log(result);
